3db16b7c553e0d5e7a0b46e33fd986d8c2513790
なぴぃ <88929313+napiiey@users.noreply.github.com>
2025-08-02 21:42:59 +0900

    update SoundSystemSettings

--- Runtime/SoundSystemSettings.cs
+++ Runtime/SoundSystemSettings.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 namespace Acfeel.SoundSystem
 {
-    [CreateAssetMenu(fileName = "SoundSystemSettings", menuName = "SoundSystem/Create Settings")]
     public class SoundSystemSettings : ScriptableObject
     {
         [Tooltip("Check this to load all audio clips at startup. It does not work when addressables enabled.")]
